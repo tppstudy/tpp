990534e401db81fe53e0ced46dc8448ae342f5df
kaituojimo <312982462@qq.com>
2018-09-27 00:37:35 +0800

    导入城市脚本和城市数据源

--- App/apis.py
+++ App/apis.py
@@ -1,7 +1,5 @@
-
 from flask_restful import Api, Resource, fields, marshal_with, reqparse
-
-
+from App.models import Letter
 
 api = Api()
 
@@ -9,3 +7,10 @@ api = Api()
 def init_api(app):
     api.init_app(app=app)
 
+
+class AreaResource(Resource):
+    def get(self):
+        return {"returnCode": "0"}
+
+
+api.add_resource(AreaResource, "/areas/")
--- App/models.py
+++ App/models.py
@@ -1,4 +1,21 @@
 from App.ext import db
 
 
-
+"""
+            {
+                "id": 454,
+                "parentId": 0,
+                "regionName": "安阳",
+                "cityCode": 410500,
+                "pinYin": "ANYANG"
+            }
+"""
+class Letter(db.Model):
+    id = db.Column(db.Integer,primary_key=True,autoincrement = True)
+    letter = db.Column(db.String(1))
+class City(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    regionName = db.Column(db.String(16))
+    cityCode = db.Column(db.Integer)
+    pinYin = db.Column(db.String(128))
+    c_letter = db.Column(db.Integer,db.ForeignKey(Letter.id))
